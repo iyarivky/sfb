94331dd608961184837040d3d4305c5fcc252c1b
Iya Rivvikyn <101973571+iyarivky@users.noreply.github.com>
2023-08-04 01:41:13 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+cd /sdcard/Download && curl -LO https://github.com/SagerNet/sing-box/releases/download/v1.3.5/sing-box-1.3.5-android-arm64.tar.gz && tar -xf sing-box-1.3.5-android-arm64.tar.gz && mv sing-box-1.3.5-android-arm64 sfb && cp /sdcard/Download/sfb /data/local/tmp && chmod +x /data/local/tmp/sfb/sing-box
