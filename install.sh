c67cdf06af6f915c3edb25bd0dc01eb34cbb69f3
Iya Rivvikyn <101973571+iyarivky@users.noreply.github.com>
2023-08-05 10:35:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,13 @@ tar -xf sing-box-1.3.5-android-arm64.tar.gz
 echo "rename folder to sfb"
 mv sing-box-1.3.5-android-arm64 sfb
 
+echo "download shell script to sfb folder"
+cd /sdcard/sing-box/sfb
+curl -LO https://raw.githubusercontent.com/iyarivky/sfb/main/re-install.sh
+curl -LO https://raw.githubusercontent.com/iyarivky/sfb/main/start.sh
+curl -LO https://raw.githubusercontent.com/iyarivky/sfb/main/stop.sh
+curl -LO https://raw.githubusercontent.com/iyarivky/sfb/main/install.sh
+
 echo "copy sfb folder to /data/local/tmp"
 cp /sdcard/sing-box/sfb /data/local/tmp
 
